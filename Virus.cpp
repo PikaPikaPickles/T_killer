8a8a70db5a5dd0ac60841103659dc5e57b5485bb
yus <ybojamtov@yandex.ua>
2020-05-24 09:38:05 +0300

    ok

---
+++ Virus.cpp
@@ -0,0 +1,10 @@
+//
+// Created by yus on 24.05.2020.
+//
+
+#include "Virus.h"
+    Virus:: Virus(){
+        dx=rand()%8-4;
+        dy=rand()%8-4;
+        name="asteroid";
+    }
---
+++ Virus.h
@@ -0,0 +1,14 @@
+//
+// Created by yus on 24.05.2020.
+//
+
+#ifndef T_KILLER_VIRUS_H
+#define T_KILLER_VIRUS_H
+#include "Enemy.h"
+
+class Virus: public Enemy {
+    Virus();
+};
+
+
+#endif //T_KILLER_VIRUS_H
